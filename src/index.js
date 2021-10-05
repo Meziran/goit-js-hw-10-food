f3a86136badae9db10721b3c33841ddf6aa9fc73
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-10-05 22:51:40 +0300

    +

--- src/index.js
+++ src/index.js
@@ -1,1 +1,10 @@
+import srcMenu from './templates/gallery.hbs';
+import menuEl from './menu.json';
 
+const createMenu = document.querySelector('.js-menu');
+
+createMenu.insertAdjacentHTML('beforeend', menuCart(menuEl));
+
+function menuCart(menuEl) {
+  return srcMenu(menuEl);
+}
