05ea1ed7dba57bbae94ab0caae36d1a9a989011d
Mateichuk_Oleksandr <71691100+Meziran@users.noreply.github.com>
2021-10-06 18:56:47 +0300

    switcher

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import srcMenu from './templates/gallery.hbs';
 import menuEl from './menu.json';
-
+// import style from './style.css';
 const createMenu = document.querySelector('.js-menu');
 
 createMenu.insertAdjacentHTML('beforeend', menuCart(menuEl));
@@ -8,3 +8,14 @@ createMenu.insertAdjacentHTML('beforeend', menuCart(menuEl));
 function menuCart(menuEl) {
   return srcMenu(menuEl);
 }
+//switchTheme
+
+const theme = document.querySelector('body');
+const switcher = document.querySelector('.theme-switch__toggle');
+
+switcher.addEventListener('click', switchTheam);
+function switchTheam() {
+  theme.classList.toggle('dark-theme');
+}
+
+//LocaleStorage
